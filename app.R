7fb46df2851226f199c4e50012993d93cd2cfe4f
Devin McCabe <devin.mccabe@gmail.com>
2019-02-12 13:51:19 -0500

    add dplyr

--- app.R
+++ app.R
@@ -1,17 +1,9 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(dplyr)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
+
    # Application title
    titlePanel("Old Faithful Geyser Data"),
 
@@ -26,7 +18,7 @@ ui <- fluidPage(
                      max = 50,
                      value = 30)
       ),
-      
+
       # Show a plot of the generated distribution
       mainPanel(
          plotOutput("distPlot")
@@ -36,17 +28,17 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
+
    output$distPlot <- renderPlot({
       # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
+      x    <- faithful[, 2]
       bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
+
       # draw the histogram with the specified number of bins
       hist(x, breaks = bins, col = 'darkgray', border = 'white')
    })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
 
